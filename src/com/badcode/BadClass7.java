ccf5ad792c41b9cbad01eb051fbab7b4b49ec827
riya.pulusuganti <riya.pulusuganti@servicenow.com>
2025-03-18 18:25:07 +0530

    vul pro

---
+++ src/com/badcode/BadClass7.java
@@ -0,0 +1,127 @@
+
+package com.badcode;
+
+public class BadClass7 {
+    public void doStuff() {
+        int x = 1, y = 2, z = 3; int unused = 99;
+        System.out.println(x+y+z);
+        for (int i = 0; i < 100; i++) {
+            for (int j = 0; j < 100; j++) {
+                for (int k = 0; k < 100; k++) {
+                    try {
+                        int a = i + j + k;
+                    } catch (Exception e) {
+                    }
+                }
+            }
+        }
+    }
+
+    public void doDuplicateStuff() {
+        doStuff(); doStuff(); doStuff(); doStuff(); doStuff();
+        doStuff(); doStuff(); doStuff(); doStuff(); doStuff();
+    }
+
+    public void longMethod() {
+        int a1 = 1; a1++;
+        int a2 = 2; a2++;
+        int a3 = 3; a3++;
+        int a4 = 4; a4++;
+        int a5 = 5; a5++;
+        int a6 = 6; a6++;
+        int a7 = 7; a7++;
+        int a8 = 8; a8++;
+        int a9 = 9; a9++;
+        int a10 = 10; a10++;
+        int a11 = 11; a11++;
+        int a12 = 12; a12++;
+        int a13 = 13; a13++;
+        int a14 = 14; a14++;
+        int a15 = 15; a15++;
+        int a16 = 16; a16++;
+        int a17 = 17; a17++;
+        int a18 = 18; a18++;
+        int a19 = 19; a19++;
+        int a20 = 20; a20++;
+        int a21 = 21; a21++;
+        int a22 = 22; a22++;
+        int a23 = 23; a23++;
+        int a24 = 24; a24++;
+        int a25 = 25; a25++;
+        int a26 = 26; a26++;
+        int a27 = 27; a27++;
+        int a28 = 28; a28++;
+        int a29 = 29; a29++;
+        int a30 = 30; a30++;
+        int a31 = 31; a31++;
+        int a32 = 32; a32++;
+        int a33 = 33; a33++;
+        int a34 = 34; a34++;
+        int a35 = 35; a35++;
+        int a36 = 36; a36++;
+        int a37 = 37; a37++;
+        int a38 = 38; a38++;
+        int a39 = 39; a39++;
+        int a40 = 40; a40++;
+        int a41 = 41; a41++;
+        int a42 = 42; a42++;
+        int a43 = 43; a43++;
+        int a44 = 44; a44++;
+        int a45 = 45; a45++;
+        int a46 = 46; a46++;
+        int a47 = 47; a47++;
+        int a48 = 48; a48++;
+        int a49 = 49; a49++;
+        int a50 = 50; a50++;
+        int a51 = 51; a51++;
+        int a52 = 52; a52++;
+        int a53 = 53; a53++;
+        int a54 = 54; a54++;
+        int a55 = 55; a55++;
+        int a56 = 56; a56++;
+        int a57 = 57; a57++;
+        int a58 = 58; a58++;
+        int a59 = 59; a59++;
+        int a60 = 60; a60++;
+        int a61 = 61; a61++;
+        int a62 = 62; a62++;
+        int a63 = 63; a63++;
+        int a64 = 64; a64++;
+        int a65 = 65; a65++;
+        int a66 = 66; a66++;
+        int a67 = 67; a67++;
+        int a68 = 68; a68++;
+        int a69 = 69; a69++;
+        int a70 = 70; a70++;
+        int a71 = 71; a71++;
+        int a72 = 72; a72++;
+        int a73 = 73; a73++;
+        int a74 = 74; a74++;
+        int a75 = 75; a75++;
+        int a76 = 76; a76++;
+        int a77 = 77; a77++;
+        int a78 = 78; a78++;
+        int a79 = 79; a79++;
+        int a80 = 80; a80++;
+        int a81 = 81; a81++;
+        int a82 = 82; a82++;
+        int a83 = 83; a83++;
+        int a84 = 84; a84++;
+        int a85 = 85; a85++;
+        int a86 = 86; a86++;
+        int a87 = 87; a87++;
+        int a88 = 88; a88++;
+        int a89 = 89; a89++;
+        int a90 = 90; a90++;
+        int a91 = 91; a91++;
+        int a92 = 92; a92++;
+        int a93 = 93; a93++;
+        int a94 = 94; a94++;
+        int a95 = 95; a95++;
+        int a96 = 96; a96++;
+        int a97 = 97; a97++;
+        int a98 = 98; a98++;
+        int a99 = 99; a99++;
+        int a100 = 100; a100++;
+    }
+}
